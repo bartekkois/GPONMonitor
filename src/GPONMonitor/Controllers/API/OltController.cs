c3796e17dfa292fbb922b65272de31a836e8f063
bartekkois <bartek.kois@gmail.com>
2016-10-23 13:17:51 +0200

    Added OLT controllers

---
+++ src/GPONMonitor/Controllers/API/OltController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GPONMonitor.Controllers
+{
+    [Route("api/[controller]")]
+    public class OltApiController : Controller
+    {
+        // GET: api/values
+        //[HttpGet]
+        //public  Task<JsonResult> Get()
+        //{
+        //    var results =  "value1";
+
+        //    return Json(results);
+        //}
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
---
+++ src/GPONMonitor/Controllers/OltController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GPONMonitor.Models.Configuration;
+using Microsoft.Extensions.Options;
+using GPONMonitor.Services;
+using System;
+
+namespace GPONMonitor.Controllers
+{
+    public class OltController : Controller
+    {
+        private readonly DevicesConfiguration _devicesConfiguration;
+        private ISnmpDataService _snmpDataService;
+
+        public OltController(IOptions<DevicesConfiguration> devicesConfiguration, ISnmpDataService snmpDataService)
+        {
+            _devicesConfiguration = devicesConfiguration.Value;
+            _snmpDataService = snmpDataService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var description = await _snmpDataService.GetOltDescriptionAsync(2);
+                var uptime = await _snmpDataService.GetOltUptimeAsync(2);
+                var onulist = await _snmpDataService.GetOnuListAsync(2);
+                var onudetail = await _snmpDataService.GetOnuDetailInfoAsync(2, 1, 2);
+
+                ViewData["Message"] = description + uptime;
+                ViewData["onulist"] = onulist;
+                ViewData["onudetail"] = onudetail;
+            }
+            catch (Exception ex)
+            {
+                ViewData["Message"] = ex.Message;
+            }
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
