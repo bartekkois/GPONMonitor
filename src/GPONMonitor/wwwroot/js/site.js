c275ff10b0ce5592f6d7eca4d04ddade1f3d78bb
bartekkois <bartek.kois@gmail.com>
2016-11-25 20:17:43 +0100

    Added html and css layout for main page

--- src/GPONMonitor/wwwroot/js/site.js
+++ src/GPONMonitor/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿// Write your Javascript code.
+﻿var TooltipWhenOverflow = (function () {
+    $(document).on('mouseenter', '.onu-list-id, .onu-list-item, .onu-detail-description, .onu-detail-item', function () {
+        var $this = $(this);
+
+        if (this.offsetWidth < this.scrollWidth && !$this.attr('title')) {
+            $this.attr('title', $this.text());
+        }
+    });
+}());
