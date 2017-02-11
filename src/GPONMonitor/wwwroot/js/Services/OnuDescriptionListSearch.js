085fe0d293ab3046e6b0753b9a808b2b26babf78
bartekkois <bartek.kois@gmail.com>
2017-02-11 14:22:44 +0100

    Added support for OLT Port ID and ONU ID filtering

--- src/GPONMonitor/wwwroot/js/Services/OnuDescriptionListSearch.js
+++ src/GPONMonitor/wwwroot/js/Services/OnuDescriptionListSearch.js
@@ -27,8 +27,23 @@
         searchForm.on("inputchange", function () {
             var searchTerm = $("#search-form").val().toLowerCase();
             if (searchTerm !== "") {
+                var searchTermOnuId = "";
+                var searchTermOnuName = "";
+                var searchTermsArray = searchTerm.split('_');
+
+                if (searchTermsArray.length >= 2)
+                {
+                    searchTermOnuId = searchTermsArray[0];
+                    searchTermOnuName = searchTermsArray[1];
+                }
+                else
+                {
+                    searchTermOnuId = "";
+                    searchTermOnuName = searchTermsArray[0];
+                }
+
                 onuListTableTbody.find("tr").each(function () {
-                    if (~$(this).find("td.onu-list-item").text().toLowerCase().indexOf(searchTerm) || ~$(this).find("td.onu-list-sn > i").attr("title").toLowerCase().indexOf(searchTerm))
+                    if (~$(this).find("td.onu-list-id").text().toLowerCase().indexOf(searchTermOnuId) && (~$(this).find("td.onu-list-item").text().toLowerCase().indexOf(searchTermOnuName) || ~$(this).find("td.onu-list-sn > i").attr("title").toLowerCase().indexOf(searchTermOnuName)))
                         $(this).removeClass("hidden");
                     else
                         $(this).addClass("hidden");
