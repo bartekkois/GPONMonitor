f4b621cff77c47ea7b0b12035128e3bbab3320b2
bartekkois <bartek.kois@gmail.com>
2017-07-15 17:08:19 +0200

    Fixed "Wieksza liczba urządzeń #1" issue

---
+++ src/GPONMonitor/wwwroot/js/Services/NavbarMediaResize.js
@@ -0,0 +1,16 @@
+﻿var NavbarMediaResize = (function () {
+    "use strict";
+
+    var init = function () {
+        function autocollapse() {
+            $('body').css({ paddingTop: $('#main-navbar').height() + 15 });
+        }
+
+        $(document).on('ready', autocollapse);
+        $(window).on('resize', autocollapse);
+    };
+
+    return {
+        init: init
+    };
+}());
--- src/GPONMonitor/wwwroot/js/site.js
+++ src/GPONMonitor/wwwroot/js/site.js
@@ -78,6 +78,22 @@ var TooltipWhenOverflow = (function () {
         init: init
     };
 }());
+var NavbarMediaResize = (function () {
+    "use strict";
+
+    var init = function () {
+        function autocollapse() {
+            $('body').css({ paddingTop: $('#main-navbar').height() + 15 });
+        }
+
+        $(document).on('ready', autocollapse);
+        $(window).on('resize', autocollapse);
+    };
+
+    return {
+        init: init
+    };
+}());
 var OltDescriptionListService = function () {
     "use strict";
 
