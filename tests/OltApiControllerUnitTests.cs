539d9e4386fab834d260e42ae7a951083f20d4f0
bartekkois <bartek.kois@gmail.com>
2018-09-08 15:05:22 +0200

    Added tests project for unit testing

---
+++ tests/OltApiControllerUnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace tests
+{
+    public class OltApiControllerUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/OnuApiControllerUnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace tests
+{
+    public class OnuApiControllerUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
