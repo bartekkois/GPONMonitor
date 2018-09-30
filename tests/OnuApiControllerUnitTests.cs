7aa59e89a13d1ac8ccdb543e760cf3d05958018d
bartekkois <bartek.kois@gmail.com>
2018-09-30 10:40:34 +0200

    Added OnuApiController unit tests

--- tests/OnuApiControllerUnitTests.cs
+++ tests/OnuApiControllerUnitTests.cs
@@ -1,14 +1,123 @@
-using System;
+using FluentAssertions;
+using GPONMonitor.Controllers;
+using GPONMonitor.Models.ComplexStateTypes;
+using GPONMonitor.Models.Configuration;
+using GPONMonitor.Models.Onu;
+using GPONMonitor.Models;
+using GPONMonitor.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
+using System;
 
 namespace tests
 {
     public class OnuApiControllerUnitTests
     {
-        [Fact]
-        public void Test1()
+        private static void ArrangeOltConfigurationsWithOnusState(out Mock<IOptions<DevicesConfiguration>> devicesConfigurationMock, out Mock<IDataService> dataServiceMock)
         {
+            // Arrange
+            devicesConfigurationMock = new Mock<IOptions<DevicesConfiguration>>();
+            devicesConfigurationMock.Setup(x => x.Value).Returns(new DevicesConfiguration()
+            {
+                Devices = new List<OltConfiguration>()
+                {
+                    new OltConfiguration{ Id = 0, Name ="GPON OLT A", IpAddress="10.1.1.21",SnmpPort ="161", SnmpVersion="2", SnmpCommunity="public", SnmpTimeout="3000"},
+                    new OltConfiguration{ Id = 1, Name ="GPON OLT B", IpAddress="10.1.1.11",SnmpPort ="161", SnmpVersion="2", SnmpCommunity="public", SnmpTimeout="3000"},
+                    new OltConfiguration{ Id = 2, Name ="GPON OLT C", IpAddress="10.2.1.11",SnmpPort ="161", SnmpVersion="2", SnmpCommunity="public", SnmpTimeout="3000"}
+                }
+            });
 
+            dataServiceMock = new Mock<IDataService>();
+            dataServiceMock.Setup(x => x.GetOnuStateAsync(0, 1, 2)).ReturnsAsync(() => new H645GOnu()
+            {
+                OltId = 0,
+                OltPortId = 1,
+                OltOnuId = 1,
+                ModelType = new ComplexStringType("H645G", "H645G", SeverityLevel.Default),
+                DescriptionName = new ComplexStringType("ONU_A", "ONU_A", SeverityLevel.Default),
+                GponSerialNumber = new ComplexStringType("DSNWcbd38d4e", "DSNWcbd38d4e", SeverityLevel.Default),
+                GponProfile = new ComplexStringType("ONU-PROFILE-INT-VLAN100", "ONU-PROFILE-INT-VLAN100", SeverityLevel.Default),
+                FirmwareVersion = new ComplexStringType("2.45", "2.45", SeverityLevel.Default),
+
+                OpticalConnectionState = new ComplexIntType(2, "aktywny", SeverityLevel.Success),
+                OpticalConnectionDeactivationReason = new ComplexIntType(1, "zanik zasilania (DGi)", SeverityLevel.Danger),
+                OpticalPowerReceived = new ComplexStringType("-215", "-21,5 dBm", SeverityLevel.Success),
+                OpticalCableDistance = new ComplexIntType(1115, "1115 m", SeverityLevel.Default),
+
+                OpticalConnectionUptime = new ComplexIntType(18844321, "218d 02:32:01", SeverityLevel.Default),
+                OpticalConnectionInactiveTime = new ComplexIntType(0, "0d 00:00:00", SeverityLevel.Default),
+                SystemUptime = new ComplexIntType(18844372, "218d 02:32:52", SeverityLevel.Default),
+
+                BlockStatus = new ComplexIntType(255, "brak blokady", SeverityLevel.Success),
+                BlockReason = new ComplexIntType(255, "brak blokady", SeverityLevel.Success),
+
+                EthernetPort1State = new ComplexIntType(1, "up", SeverityLevel.Success),
+                EthernetPort1Speed = new ComplexIntType(3, "1000 Mb/s", SeverityLevel.Success)
+            });
+
+            dataServiceMock.Setup(x => x.GetOnuStateAsync(0, 1, 1)).Throws(new Exception());
+        }
+
+
+        [Fact]
+        public async Task GetH645GOnuStateAsync_ShouldReturnOnuStateIfOnuExists_Async()
+        {
+            // Arrange
+            ArrangeOltConfigurationsWithOnusState(out Mock<IOptions<DevicesConfiguration>> devicesConfigurationMock, out Mock<IDataService> dataServiceMock);
+
+            var controller = new OnuApiController(devicesConfigurationMock.Object, dataServiceMock.Object);
+
+            // Act
+            var result = await controller.GetOnuStateAsync(0, 1, 2);
+
+            // Assert
+            var okResult = result.Should().BeOfType<JsonResult>().Subject;
+            var onu = okResult.Value.Should().BeAssignableTo<H645GOnu>().Subject;
+
+            onu.OltId.Should().Be(0);
+            onu.OltPortId.Should().Be(1);
+            onu.OltOnuId.Should().Be(1);
+
+            onu.ModelType.Should().BeEquivalentTo(new ComplexStringType("H645G", "H645G", SeverityLevel.Default));
+            onu.DescriptionName.Should().BeEquivalentTo(new ComplexStringType("ONU_A", "ONU_A", SeverityLevel.Default));
+            onu.GponSerialNumber.Should().BeEquivalentTo(new ComplexStringType("DSNWcbd38d4e", "DSNWcbd38d4e", SeverityLevel.Default));
+            onu.GponProfile.Should().BeEquivalentTo(new ComplexStringType("ONU-PROFILE-INT-VLAN100", "ONU-PROFILE-INT-VLAN100", SeverityLevel.Default));
+            onu.FirmwareVersion.Should().BeEquivalentTo(new ComplexStringType("2.45", "2.45", SeverityLevel.Default));
+
+            onu.OpticalConnectionState.Should().BeEquivalentTo(new ComplexIntType(2, "aktywny", SeverityLevel.Success));
+            onu.OpticalConnectionDeactivationReason.Should().BeEquivalentTo(new ComplexIntType(1, "zanik zasilania (DGi)", SeverityLevel.Danger));
+            onu.OpticalPowerReceived.Should().BeEquivalentTo(new ComplexStringType("-215", "-21,5 dBm", SeverityLevel.Success));
+            onu.OpticalCableDistance.Should().BeEquivalentTo(new ComplexIntType(1115, "1115 m", SeverityLevel.Default));
+
+            onu.OpticalConnectionUptime.Should().BeEquivalentTo(new ComplexIntType(18844321, "218d 02:32:01", SeverityLevel.Default));
+            onu.OpticalConnectionInactiveTime.Should().BeEquivalentTo(new ComplexIntType(0, "0d 00:00:00", SeverityLevel.Default));
+            onu.SystemUptime.Should().BeEquivalentTo(new ComplexIntType(18844372, "218d 02:32:52", SeverityLevel.Default));
+
+            onu.BlockStatus.Should().BeEquivalentTo(new ComplexIntType(255, "brak blokady", SeverityLevel.Success));
+            onu.BlockReason.Should().BeEquivalentTo(new ComplexIntType(255, "brak blokady", SeverityLevel.Success));
+
+            onu.EthernetPort1State.Should().BeEquivalentTo(new ComplexIntType(1, "up", SeverityLevel.Success));
+            onu.EthernetPort1Speed.Should().BeEquivalentTo(new ComplexIntType(3, "1000 Mb/s", SeverityLevel.Success));
+        }
+        
+
+        [Fact]
+        public async Task GetH645GOnuStateAsync_ShouldReturnNotFoundIfOnuDoesntExists_Async()
+        {
+            // Arrange
+            ArrangeOltConfigurationsWithOnusState(out Mock<IOptions<DevicesConfiguration>> devicesConfigurationMock, out Mock<IDataService> dataServiceMock);
+
+            var controller = new OnuApiController(devicesConfigurationMock.Object, dataServiceMock.Object);
+
+            // Act
+            var result = await controller.GetOnuStateAsync(0, 1, 1);
+
+            // Assert
+            var okResult = result.Should().BeOfType<NotFoundObjectResult>();
         }
     }
 }
