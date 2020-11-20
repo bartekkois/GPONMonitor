bd269b75db9f77946be270ce2fd59ccbd4ab12e8
bartekkois <bartek.kois@gmail.com>
2020-11-20 18:01:47 +0100

    Fixed calculatedTimeSpanAndDateTime hour format

--- src/GPONMonitor/Utils/Utils.cs
+++ src/GPONMonitor/Utils/Utils.cs
@@ -6,10 +6,10 @@ namespace GPONMonitor.Utils
     {
         public static string CalculateTimeSpanAndDateTimeFormSeconds(int? timeSpanInSeconds)
         {
-            var calculatedTimeSpanAndDateTime = TimeSpan.FromSeconds(Convert.ToDouble(timeSpanInSeconds)).ToString(@"d\d\ hh\:mm\:ss");
+            var calculatedTimeSpanAndDateTime = TimeSpan.FromSeconds(Convert.ToDouble(timeSpanInSeconds)).ToString(@"d\d\ h\:mm\:ss");
 
             if (timeSpanInSeconds != 0)
-                calculatedTimeSpanAndDateTime = calculatedTimeSpanAndDateTime + " " + (DateTime.Now - TimeSpan.FromSeconds(Convert.ToDouble(timeSpanInSeconds))).ToString("(d.MM HH:mm:ss)");
+                calculatedTimeSpanAndDateTime = calculatedTimeSpanAndDateTime + " " + (DateTime.Now - TimeSpan.FromSeconds(Convert.ToDouble(timeSpanInSeconds))).ToString("(d.MM H:mm:ss)");
 
             return calculatedTimeSpanAndDateTime;
         }
