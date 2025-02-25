e5f389f79cc3abb99ab686573f37d9782df2ab99
bartekkois <bartek.kois@gmail.com>
2025-02-25 06:24:57 +0100

    Added support for Halny HL-1B and HL-4BX3v-F ONUs

---
+++ src/GPONMonitor/Models/Onu/HL1BOnu.cs
@@ -0,0 +1,11 @@
+﻿using GPONMonitor.Models.ComplexStateTypes;
+using GPONMonitor.Models.OnuFactory;
+
+namespace GPONMonitor.Models.Onu
+{
+    public class HL1BOnu : GenericOnu, IOnuFactory
+    {
+        public ComplexIntType EthernetPort1State { get; set; }
+        public ComplexIntType EthernetPort1Speed { get; set; }
+    }
+}
